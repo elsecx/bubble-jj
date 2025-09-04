1a5612ebf6a20f9e187722c127f6a17fb774d869
elsecx <alwafielgin@gmail.com>
2025-09-05 00:43:25 +0700

    feat: create model & migration for data_jj

---
+++ database/migrations/2025_09_05_003225_create_data_j_j_s_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data_jj', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('username1');
+            $table->string('username2')->nullable();
+            $table->string('filename');
+            $table->integer('duration');
+            $table->integer('size');
+            $table->boolean('sts_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data_jj');
+    }
+};
