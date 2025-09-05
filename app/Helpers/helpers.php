9521416ddcefe32e55684762e1f39205e030f8fe
elsecx <alwafielgin@gmail.com>
2025-09-05 11:03:12 +0700

    feat: create helper for format file size

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -22,3 +22,18 @@ if (!function_exists('spaRender')) {
     }
   }
 }
+
+if (!function_exists('formatSize')) {
+  function formatSize($bytes, $precision = 2)
+  {
+    $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+    $bytes = max($bytes, 0);
+    $pow = floor(($bytes ? log($bytes, 1024) : 0));
+    $pow = min($pow, count($units) - 1);
+
+    $bytes /= (1 << (10 * $pow));
+
+    return round($bytes, $precision) . $units[$pow];
+  }
+}
